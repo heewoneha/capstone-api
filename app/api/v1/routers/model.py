a0287e4e69dbb439f0512c46243723ec5732f9ac
heewoneha <jhjmo0719h@naver.com>
2025-06-01 20:46:11 +0900

    feat: use public blob storage for the result

--- app/api/v1/routers/model.py
+++ app/api/v1/routers/model.py
@@ -3,7 +3,7 @@
 from fastapi import Header, HTTPException, APIRouter
 from uuid import UUID
 from http import HTTPStatus
-from app.services.constant import DanceName, BACKGROUND_DIR, CHARACTER_DIR, RESULT_DIR, MODEL_SOURCE_DIR, MODEL_RESULT_DIR
+from app.services.constant import DanceName, AZURE_PUBLIC_STORAGE_CONTAINER_NAME, AZURE_PUBLIC_CONNECTION_STRING, BACKGROUND_DIR, CHARACTER_DIR, RESULT_DIR, MODEL_SOURCE_DIR, MODEL_RESULT_DIR
 from app.services.blob import AzureBlobService
 from app.services.run_model import delete_tmp_result_files, delete_tmp_source_files, image_to_animation
 
@@ -19,13 +19,15 @@ async def handle_model_request(x_cd_user_id: str = Header(...)):
     
     user_uuid = str(user_uuid)
 
-    BlobService = AzureBlobService()
+    PrivateBlobService = AzureBlobService()
 
     try:
-        BlobService.get_result_image(save_path=f"{MODEL_SOURCE_DIR}/{BACKGROUND_DIR}/{user_uuid}.png", blob_name=f"{BACKGROUND_DIR}/{user_uuid}.png")
-        BlobService.get_result_image(save_path=f"{MODEL_SOURCE_DIR}/{CHARACTER_DIR}/{user_uuid}.png", blob_name=f"{CHARACTER_DIR}/{user_uuid}.png")
+        PrivateBlobService.get_result_image(save_path=f"{MODEL_SOURCE_DIR}/{BACKGROUND_DIR}/{user_uuid}.png", blob_name=f"{BACKGROUND_DIR}/{user_uuid}.png")
+        PrivateBlobService.get_result_image(save_path=f"{MODEL_SOURCE_DIR}/{CHARACTER_DIR}/{user_uuid}.png", blob_name=f"{CHARACTER_DIR}/{user_uuid}.png")
     except Exception as e:
         raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=f"Error downloading images from Blob: {str(e)}")
+
+    PublicBlobService = AzureBlobService(container_name=AZURE_PUBLIC_STORAGE_CONTAINER_NAME, connecting_string=AZURE_PUBLIC_CONNECTION_STRING)
 
     try:
         for dance_name in DanceName:
@@ -33,8 +35,8 @@ async def handle_model_request(x_cd_user_id: str = Header(...)):
                 user_uuid=user_uuid,
                 dance_name=dance_name,
             )
-            BlobService.upload_file(file_path=f"{MODEL_RESULT_DIR}/{user_uuid}/video.gif", blob_name=f"{RESULT_DIR}/{dance_name.value}/{user_uuid}.gif")
-            BlobService.upload_file(file_path=f"{MODEL_RESULT_DIR}/{user_uuid}/video.mp4", blob_name=f"{RESULT_DIR}/{dance_name.value}/{user_uuid}.mp4")
+            PublicBlobService.upload_file(file_path=f"{MODEL_RESULT_DIR}/{user_uuid}/video.gif", blob_name=f"{RESULT_DIR}/{dance_name.value}/{user_uuid}.gif")
+            PublicBlobService.upload_file(file_path=f"{MODEL_RESULT_DIR}/{user_uuid}/video.mp4", blob_name=f"{RESULT_DIR}/{dance_name.value}/{user_uuid}.mp4")
             delete_tmp_result_files(user_uuid=user_uuid)
         delete_tmp_source_files(user_uuid=user_uuid)
     except Exception as e:
